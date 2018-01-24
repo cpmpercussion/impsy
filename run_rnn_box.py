006aa4455c90557413eb6d25dac0fd719c19affa
Charles Martin <cpm@charlesmartin.com.au>
2018-01-24 16:44:35 +0100

    Non working commit of new box script
    Former-commit-id: 0d1e92ffd421e8add462635ec5637aaaa0030a1b

--- run_rnn_box.py
+++ run_rnn_box.py
@@ -11,16 +11,21 @@ from threading import Thread
 import sketch_mdn
 import numpy as np
 import tensorflow as tf
+import queue
 
 # Input and output to serial are bytes (0-255)
 # Output to Pd is a float (0-1)
+parser = argparse.ArgumentParser(description='Interface for RNN Box.')
 
-parser = argparse.ArgumentParser(description='Runs the RNN interaction box interface, use the command line to change modes.')
+
 parser.add_argument('-l', '--log', dest='logging', action="store_true", help='Save input and RNN data to a log file.')
 parser.add_argument('-t', '--test', dest='test', action="store_true", help='No RNN, user input only directly connected to servo.')
 parser.add_argument('-c', '--call', dest='callresponse', action="store_true", help='Call and response mode.')
-parser.add_argument('-p', '--polyphony', dest='polyphone', action="store_true", help='Harmony mode.')
+parser.add_argument('-p', '--polyphony', dest='polyphony', action="store_true", help='Harmony mode.')
 parser.add_argument('-b', '--battle', dest='battle', action="store_true", help='Battle royale mode.')
+parser.add_argument('-u', '--user', dest='usermodel', action="store_true", help='Use human RNN model.')
+parser.add_argument('-s', '--synthetic', dest='syntheticmodel', action='store_true', help='Use synthetic RNN model.')
+
 args = parser.parse_args()
 
 LOG_FILE = datetime.datetime.now().isoformat().replace(":", "-")[:19] + "-rnnbox.log"  # Log file name.
@@ -112,29 +117,72 @@ sketch_mdn.MODEL_DIR = "./"
 # Main network
 net = sketch_mdn.MixtureRNN(mode=sketch_mdn.NET_MODE_RUN, n_hidden_units=128, n_mixtures=10, batch_size=1, sequence_length=1)
 print("RNN Loaded.")
+rnn_output_buffer = queue.Queue()
 
 
 # Process Args here
 
-if args.logging:
-    logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format=LOG_FORMAT)
-    print("Logging enabled:", LOG_FILE)
 
 # Parameters
-thread_running = True
-user_to_sound = True
+# All set to false before setting is chosen.
+thread_running = False
+user_to_sound = False
 user_to_rnn = False
 rnn_to_rnn = False
 rnn_to_servo = False
 user_to_servo = True
 rnn_to_sound = False
-
 listening_as_well = False
-first_touch = np.array([(0.01 + (np.random.rand() - 0.5) * 0.005), (np.random.rand() - 0.5)])
 
 
-def run_loop():
-    # Reacts to serial with sound, not movement
+def random_touch():
+    return np.array([(0.01 + (np.random.rand() - 0.5) * 0.005), (np.random.rand() - 0.5)])
+
+# Touch storage for RNN.
+last_rnn_touch = random_touch()  # prepare previous touch input for RNN input
+last_user_touch = random_touch()
+last_user_interaction = time.time()
+
+
+if args.logging:
+    logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format=LOG_FORMAT)
+    print("Logging enabled:", LOG_FILE)
+
+
+# Interactive Mapping
+if args.test:
+    print("Entering test mode (no RNN).")
+    user_to_sound = True
+    user_to_servo = False
+elif args.callresponse:
+    print("Entering call and response mode.")
+elif args.polyphony:
+    print("Entering polyphony mode.")
+elif args.battle:
+    print("Entering battle royale mode.")
+
+# TODO make this work.
+# RNN Model choice
+if args.usermodel:
+    print("Using human RNN model.")
+elif args.syntheticmodel:
+    print("Using synthetic RNN model.")
+
+
+def rnn_make_prediction(sess):
+    input = None
+    if user_to_rnn:
+        input = last_user_touch
+    if rnn_to_rnn:
+        input = last_rnn_touch
+    rnn_output = net.generate_touch(input, sess)
+    rnn_output_buffer.put_nowait(rnn_output)  # put it in the playback queue.
+
+
+def playback_user_loop(sess):
+    # Plays back serial messages from the user
+    global last_user_touch
+    global last_user_interaction
     while thread_running:
         loc = None
         while ser.in_waiting > 0:
@@ -143,12 +191,56 @@ def run_loop():
             send_sound_command(touch_message_datagram(loc / 255.0))
         if user_to_servo and loc:
             command_servo(loc)
+        dt = 0.5
+        last_user_touch = np.array([dt, loc / 255.0])
+        last_user_interaction = time.time()
+        if user_to_rnn:
+            rnn_make_prediction(sess)
 
-# # Start a thread for listening
-# if listening_as_well:
-#     thread_running = True
-#     thread = Thread(target=threaded_function)
-#     thread.start()
+
+def playback_rnn_loop(sess):
+    # Plays back RNN notes from its buffer queue.
+    global last_rnn_touch
+    while thread_running:
+        item = rnn_output_buffer.get()  # could put a timeout here.
+        if item is not None:
+            # convert to dt, byte format
+            dt = item[0]
+            pos = item[1]
+            pos = int((pos + 10) * 255 / 20.0)  # what does this maths do?
+            pos = min(max(pos, 0), 255)  # ditto here?
+            dt = max(dt, 0)  # stop accidental minus dt
+            time.sleep(dt)  # wait until time to play the sound
+            if rnn_to_sound:
+                # RNN can be disconnected from sound
+                move_and_play_sound(pos)  # do the playing and moving
+                logging.info("{1}, rnn, {0}".format(pos, datetime.datetime.now().isoformat()))
+            last_rnn_touch = item  # Set the last_rnn_touch (after the time delay?)
+            rnn_output_buffer.task_done()
+        if rnn_to_rnn:
+            rnn_make_prediction(sess)
+
+
+CALL_RESPONSE_THRESHOLD = 2.0
+
+
+def monitor_user_action():
+    # Handles changing responsibility in Call-Response mode.
+    while thread_running:
+        # Check when the last user interaction was
+        dt = time.time() - last_user_interaction
+        if dt > CALL_RESPONSE_THRESHOLD and args.callresponse:
+            # switch call response modes.
+            user_to_rnn = False
+            rnn_to_rnn = True
+            rnn_to_sound = True
+            rnn_to_servo = True
+        elif args.callresponse:
+            user_to_rnn = True
+            rnn_to_rnn = False
+            rnn_to_sound = False
+            rnn_to_servo = False
+
 
 # # Start TF Predictions
 # with tf.Session() as sess:
@@ -181,12 +273,22 @@ def run_loop():
 #     print("thread finished...exiting")
 
 
-
 print("Now running...")
-try:
-    while True:
-        run_loop()
-except KeyboardInterrupt:
-    pass
-finally:
-    print("\nDisconnected")
+thread_running = True
+
+with tf.Session() as sess:
+    net.prepare_model_for_running(sess)
+    try:
+        while True:
+            playback_user_loop(sess)
+    except KeyboardInterrupt:
+        thread_running = False
+        pass
+    finally:
+        print("\nDisconnected")
+
+# # Start a thread for listening
+# if listening_as_well:
+#     thread_running = True
+#     thread = Thread(target=threaded_function)
+#     thread.start()
