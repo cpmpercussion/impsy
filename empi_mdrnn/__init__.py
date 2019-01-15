ab7f9c99622ccd5b03d788e7b2f8b6dd3dfbe95b
Charles Martin <cpm@charlesmartin.com.au>
2019-01-15 23:19:49 +0100

    Parametrised the output dimension for use in general interfaces

--- empi_mdrnn/__init__.py
+++ empi_mdrnn/__init__.py
@@ -21,7 +21,7 @@ LOG_PATH = "./logs/"
 SCALE_FACTOR = 10  # scales input and output from the model. Should be the same between training and inference.
 
 
-def build_model(seq_len=30, hidden_units=256, num_mixtures=5, layers=2,
+def build_model(seq_len=30, hidden_units=256, num_mixtures=5, layers=2, out_dim=2
                      time_dist=True, inference=False, compile_model=True,
                      print_summary=True):
     """Builds a EMPI MDRNN model for training or inference.
@@ -31,13 +31,13 @@ def build_model(seq_len=30, hidden_units=256, num_mixtures=5, layers=2,
     hidden_units : number of LSTM units in each layer
     num_mixtures : number of mixture components (5-10 is good)
     layers : number of layers (2 is good)
+    out_dim : number of dimensions for the model = number of degrees of freedom + 1 (time)
     time_dist : time distributed or not (default True)
     inference : inference network or training (default False)
     compile_model : compiles the model (default True)
     print_summary : print summary after creating mdoe (default True)
     """
     print("Building EMPI Model...")
-    out_dim = 2
     # Set up training mode
     stateful = False
     batch_shape = None
@@ -83,35 +83,35 @@ def load_inference_model(model_file="", layers=2, units=512, mixtures=5, predict
     return decoder
 
 
-def random_sample():
-    """ Generate a random sample in format (dt, x), where dt is positive
-    and x is between 0 and 1."""
-    return np.array([(0.01 + (np.random.rand()-0.5)*0.005), np.random.rand()])
+def random_sample(out_dim=2):
+    """ Generate a random sample in format (dt, x_1, ..., x_n), where dt is positive
+    and the x_i are between 0 and 1."""
+    output = np.random.rand(out_dim)
+    output[0] = (0.01 + (np.random.rand()-0.5)*0.005)  # TODO: see if this dt heuristic should change
+    return output
 
 
-def proc_generated_touch(touch):
+def proc_generated_touch(x_input, out_dim=2):
     """ Processes a generated touch in the format (dt, x)
         such that dt > 0, and 0 <= x <= 1 """
-    dt = max(touch[0], 0.000454)
-    x_loc = min(max(touch[1], 0), 1)
-    return np.array([dt, x_loc])
+    dt = np.maximum(x_input[0], 0.000454)  # TODO: see if the min value of dt shoud change.
+    x_output = np.minimum(np.maximum(x_input[1:], 0), 1)
+    return np.concatenate([np.array([dt]), x_output])
 
 
-def generate_sample(model, n_mixtures, prev_sample, pi_temp=1.0, sigma_temp=0.0):
+def generate_sample(model, n_mixtures, prev_sample, pi_temp=1.0, sigma_temp=0.0, out_dim=2):
     """Generate one forward prediction from a previous sample in format
-    (dt, x). Pi and Sigma temperature are adjustable."""
-    out_dim = 2
+    (dt, x_1,...,x_n). Pi and Sigma temperature are adjustable."""
     params = model.predict(prev_sample.reshape(1, 1, out_dim) * SCALE_FACTOR)
     new_sample = mdn.sample_from_output(params[0], out_dim, n_mixtures, temp=pi_temp, sigma_temp=sigma_temp) / SCALE_FACTOR
     new_sample = new_sample.reshape(out_dim,)
     return new_sample
 
 
-def generate_performance(model, n_mixtures, first_sample, time_limit=None, steps_limit=1000, pi_temp=1.0, sigma_temp=0.0):
+def generate_performance(model, n_mixtures, first_sample, time_limit=None, steps_limit=1000, pi_temp=1.0, sigma_temp=0.0, out_dim=2):
     """Generates a performance of (dt, x) pairs, up to a step_limit.
     Time limit is not presently implemented.
     """
-    out_dim = 2
     time = 0
     steps = 0
     prev_sample = first_sample
