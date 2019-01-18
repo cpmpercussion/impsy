43494b43efc55f376676cea20a22e0560452963c
Charles Martin <cpm@charlesmartin.com.au>
2019-01-18 12:44:59 +0100

    Update setup_raspberry_pi_tensorflow.sh

--- utils/setup_raspberry_pi_tensorflow.sh
+++ utils/setup_raspberry_pi_tensorflow.sh
@@ -3,4 +3,5 @@ sudo apt-get update
 sudo apt install libatlas-base-dev
 sudo apt-get install python3-numpy
 sudo apt-get install python3-pandas
-pip3 install tensorflow, keras, tensorflow-probability, python-osc
+pip3 install tensorflow, keras, python-osc
+pip3 install --no-cache-dir -I tensorflow-probability=0.4.0
