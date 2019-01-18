50e24a5a5ee4a3c900699823c8116312e313c43f
Charles Martin <cpm@charlesmartin.com.au>
2019-01-18 11:06:47 +0100

    added RPi setup script

---
+++ utils/setup_raspberry_pi_tensorflow.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install libatlas-base-dev
+sudo apt-get install python3-numpy
+sudo apt-get install python3-pandas
+pip3 install tensorflow, keras, tensorflow-probability, python-osc
