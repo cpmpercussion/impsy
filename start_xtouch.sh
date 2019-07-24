8e06aeb25622d7f5f6327d7f5696fbd20a46052a
Charles Martin <cpm@charlesmartin.com.au>
2019-07-24 17:10:05 +1000

    updates pre ACMC mini performance

--- start_xtouch.sh
+++ start_xtouch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /Applications/Pd-0.49-1.app/Contents/Resources/bin/pd midi_controllers/xtouch-mini.pd &
-python predictive_music_model.py -d=9 --modelsize=xs --log -v -c
+python3 predictive_music_model.py -d=9 --modelsize=xs --log -v -c
