ed8bd6fe5d06fa4137bc0e2bb454d6d7ac561d86
Charles Martin <cpm@charlesmartin.com.au>
2024-06-26 10:59:45 +1000

    test logging function

--- impsy/tests/test_interaction.py
+++ impsy/tests/test_interaction.py
@@ -3,3 +3,8 @@ from impsy import interaction
 def test_interaction_server():
   """Just tests creation of an interaction server object"""
   interaction_server = interaction.InteractionServer()
+
+def test_logging():
+  """Just sets up logging"""
+  interaction.setup_logging(2)
+
