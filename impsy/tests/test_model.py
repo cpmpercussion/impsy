262655f05a58f2aa1376737d5e67c55fa83ba50d
Charles Martin <cpm@charlesmartin.com.au>
2024-06-26 11:24:02 +1000

    better generate data function now accepts dimension as a parameter

--- impsy/tests/test_model.py
+++ impsy/tests/test_model.py
@@ -13,7 +13,7 @@ def test_model():
 def test_inference():
     """Test inference from a PredictiveMusicMDRNN model"""
     dimension = 8
-    num_test_steps = 20
+    num_test_steps = 5
     net = mdrnn.PredictiveMusicMDRNN(mode=mdrnn.NET_MODE_RUN, dimension=dimension)
     value = mdrnn.random_sample(out_dim=dimension)
     for i in range(num_test_steps):
@@ -24,17 +24,19 @@ def test_inference():
 def test_training():
     """Test training on a PredictiveMusicMDRNN model"""
     num_epochs = 1
-    sequence_length = 10
+    sequence_length = 3
+    batch_size = 3
+    dimension = 3
     net = mdrnn.PredictiveMusicMDRNN(
         mode=mdrnn.NET_MODE_TRAIN,
-        dimension=2,
-        n_hidden_units=32,
+        dimension=dimension,
+        n_hidden_units=16,
         n_mixtures=5,
-        batch_size=100,
+        batch_size=batch_size,
         sequence_length=sequence_length,
         layers=2,
     )
-    x_t_log = utils.generate_data(samples=((sequence_length + 1) * 10))
+    x_t_log = utils.generate_data(samples=((sequence_length + 1) * batch_size), dimension=dimension)
     slices = train.slice_sequence_examples(x_t_log, sequence_length + 1, step_size=1)
     Xs, ys = train.seq_to_overlapping_format(slices)
     history = net.train(Xs, ys, num_epochs=num_epochs, saving=False)
--- impsy/utils.py
+++ impsy/utils.py
@@ -52,40 +52,26 @@ def batch_generator(seq_len, batch_size, dim, corpus):
         yield batch_X, batch_y
 
 
-def generate_data(samples=50000):
+def fuzzy_sine_function(t, scale=1.0, fuzz_factor=0.05):
+    """A fuzzy sine function with variable fuzz factor"""
+    return np.sin(t) * scale + (np.random.normal() * fuzz_factor)
+
+
+def generate_data(samples: int = 50000, dimension: int = 2):
     """Generating some Slightly fuzzy sine wave data."""
+    assert dimension > 1, "dimension must be greater than 1"
     NSAMPLE = samples
     print("Generating", str(NSAMPLE), "toy data samples.")
     t_data = np.float32(np.array(range(NSAMPLE)) / 10.0)
     t_interval = t_data[1] - t_data[0]
-    t_r_data = np.random.normal(0, t_interval / 20.0, size=NSAMPLE)
+    t_r_data = np.random.normal(0, t_interval / 20.0, size=NSAMPLE) ## fuzz up the time sampling
     t_data = t_data + t_r_data
     r_data = np.random.normal(size=NSAMPLE)
-    x_data = np.sin(t_data) * 1.0 + (r_data * 0.05)
-    df = pd.DataFrame({"t": t_data, "x": x_data})
+    # x_data = np.sin(t_data) * 1.0 + (r_data * 0.05)
+    df = pd.DataFrame({"t": t_data})
+    for i in range(dimension - 1):
+        df[f"x{i}"] = df["t"].apply(fuzzy_sine_function, scale=i)
     df.t = df.t.diff()
     df.t = df.t.fillna(1e-4)
     print(df.describe())
     return np.array(df)
-
-
-def generate_synthetic_3D_data():
-    """
-    Generates some slightly fuzzy sine wave data
-    in two dimensions (plus time).
-    """
-    NSAMPLE = 50000
-    print("Generating", str(NSAMPLE), "toy data samples.")
-    t_data = np.float32(np.array(range(NSAMPLE)) / 10.0)
-    t_interval = t_data[1] - t_data[0]
-    t_r_data = np.random.normal(0, t_interval / 20.0, size=NSAMPLE)
-    t_data = t_data + t_r_data
-    r_data = np.random.normal(size=NSAMPLE)
-    x_data = (np.sin(t_data) + (r_data / 10.0) + 1) / 2.0
-    y_data = (np.sin(t_data * 3.0) + (r_data / 10.0) + 1) / 2.0
-    df = pd.DataFrame({"a": x_data, "b": y_data, "t": t_data})
-    df.t = df.t.diff()
-    df.t = df.t.fillna(1e-4)
-    print(df.describe())
-    return np.array(df)
-
