d01ec8396d0e356c9be888a6b3cf89785f451193
Charles Martin <cpm@charlesmartin.com.au>
2024-06-26 10:34:29 +1000

    basic tests for the CLI

---
+++ impsy/tests/test_commands.py
@@ -0,0 +1,13 @@
+from click.testing import CliRunner
+from impsy.impsy import cli
+
+
+def test_main_command():
+  runner = CliRunner()
+  result = runner.invoke(cli)
+  assert result.exit_code == 0
+
+def test_model_test_command():
+  runner = CliRunner()
+  result = runner.invoke(cli, ['test-mdrnn'])
+  # assert result.exit_code == 0
