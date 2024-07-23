d1a8ef0b2590a5b7b22c5d17833cdb3d61210700
Charles Martin <cpm@charlesmartin.com.au>
2024-07-23 23:17:48 +1000

    working on new rpi systemd service

--- examples/rpi/rpi_start_on_boot.sh
+++ examples/rpi/rpi_start_on_boot.sh
@@ -2,9 +2,9 @@
 
 # This script installs a systemd service to start the genAI program on boot.
 
-cd /home/pi/genAI-MIDI-module
-sudo cp /home/pi/genAI-MIDI-module/genaimodule.service /etc/systemd/system/genaimodule.service
-sudo chmod 644 /etc/systemd/system/genaimodule.service
-# sudo systemctl start genaimodule.service
-# sudo systemctl stop genaimodule.service
-sudo systemctl enable genaimodule.service
+cd /home/pi/imps
+sudo cp /home/pi/imps/examples/rpi/impsy.service /etc/systemd/system/impsy.service
+sudo chmod 644 /etc/systemd/system/impsy.service
+# sudo systemctl start impsy.service
+# sudo systemctl stop impsy.service
+sudo systemctl enable impsy.service
