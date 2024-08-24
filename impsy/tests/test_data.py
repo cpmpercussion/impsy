2d8ebdabb412b03cead6a1f1b0c07cd842172855
Charles Martin <cpm@charlesmartin.com.au>
2024-08-24 13:50:26 +1000

    move inference tests to test_data, need to structure this better.

--- impsy/tests/test_data.py
+++ impsy/tests/test_data.py
@@ -1,12 +1,17 @@
 from impsy import dataset
 from impsy import train
 from impsy import tflite_converter
+from impsy import utils
+from impsy import mdrnn
 import numpy as np
 import os
 import random
 import tensorflow as tf
 import pytest
+from pathlib import Path
 
+
+## MDRNN configuration
 
 @pytest.fixture(scope="session")
 def dimension():
@@ -15,6 +20,20 @@ def dimension():
 @pytest.fixture(scope="session")
 def mdrnn_size():
     return "xs"
+
+@pytest.fixture(scope="session")
+def units(mdrnn_size):
+    return utils.mdrnn_config(mdrnn_size)['units']
+
+@pytest.fixture(scope="session")
+def mixtures(mdrnn_size):
+    return utils.mdrnn_config(mdrnn_size)['mixes']
+
+@pytest.fixture(scope="session")
+def layers(mdrnn_size):
+    return utils.mdrnn_config(mdrnn_size)['layers']
+
+## Locations
 
 @pytest.fixture(scope="session")
 def dataset_location(tmp_path_factory):
@@ -102,21 +121,22 @@ def test_train_command(trained_model):
     """Test that a trained model can be constructed."""
     assert os.path.isfile(trained_model["weights_file"])
     assert os.path.isfile(trained_model["keras_file"])
+    assert os.path.isfile(trained_model["tflite_file"])
     assert isinstance(trained_model["history"], tf.keras.callbacks.History)
 
 
-### tflite tests
+### tflite conversion tests
 
-def test_config_to_tflite():
+
+def test_config_to_tflite(models_location):
     test_config = "configs/default.toml"
-    tflite_file = tflite_converter.config_to_tflite(test_config)
+    tflite_file = tflite_converter.config_to_tflite(test_config, save_path=models_location)
     assert os.path.exists(tflite_file)
 
 
-def test_weights_to_model_file(trained_model, dimension, tmp_path_factory, mdrnn_size):
+def test_weights_to_model_file(trained_model, dimension, mdrnn_size):
     weights_file = trained_model["weights_file"]
     print(f"Weights file: {weights_file}")
-    test_dir = tmp_path_factory.mktemp("model_file")
     tflite_file = tflite_converter.weights_file_to_model_file(weights_file, mdrnn_size, dimension)
     print(f"File returned: {tflite_file}")
     assert os.path.exists(tflite_file)
@@ -124,6 +144,79 @@ def test_weights_to_model_file(trained_model, dimension, tmp_path_factory, mdrnn
 
 def test_model_file_to_tflite(trained_model):
     model_filename = trained_model["keras_file"]
-    expected_output_path = model_filename.with_suffix('.tflite')
     tflite_file = tflite_converter.model_file_to_tflite(model_filename)
     assert os.path.exists(tflite_file)
+
+
+### inference model tests.
+
+
+@pytest.fixture(scope="session")
+def tflite_file(trained_model):
+    return trained_model['tflite_file']
+
+@pytest.fixture(scope="session")
+def weights_file(trained_model):
+    return trained_model['weights_file']
+
+@pytest.fixture(scope="session")
+def keras_file(trained_model):
+    return trained_model['keras_file']
+
+@pytest.fixture(scope="session")
+def tflite_model(tflite_file, dimension, units, mixtures, layers):
+    model = mdrnn.TfliteMDRNN(tflite_file, dimension, units, mixtures, layers)
+    return model
+
+def test_tflite_predictions(tflite_model: mdrnn.TfliteMDRNN):
+    """Test inference from a TfliteMDRNN model"""
+    num_test_steps = 5
+    dimension = tflite_model.dimension
+    value = mdrnn.random_sample(out_dim=dimension)
+    for i in range(num_test_steps):
+        value = tflite_model.generate(value)
+        assert len(value) == dimension
+        value = mdrnn.proc_generated_touch(value, dimension)
+        assert len(value) == dimension
+
+@pytest.fixture(scope="session")
+def keras_model(keras_file, dimension, units, mixtures, layers):
+    model = mdrnn.KerasMDRNN(keras_file, dimension, units, mixtures, layers)
+    return model
+
+def test_keras_predictions(keras_model: mdrnn.KerasMDRNN):
+    """Test inference from a KerasMDRNN model"""
+    num_test_steps = 5
+    dimension = keras_model.dimension
+    value = mdrnn.random_sample(out_dim=dimension)
+    for i in range(num_test_steps):
+        value = keras_model.generate(value)
+        assert len(value) == dimension
+        value = mdrnn.proc_generated_touch(value, dimension)
+        assert len(value) == dimension
+
+@pytest.fixture(scope="session")
+def weights_model(weights_file, dimension, units, mixtures, layers):
+    assert weights_file.suffix == ".h5", "has to be an .h5 weights"
+    model = mdrnn.KerasMDRNN(weights_file, dimension, units, mixtures, layers)
+    return model
+
+def test_weights_predictions(weights_model: mdrnn.KerasMDRNN):
+    """Test inference from a KerasMDRNN model"""
+    num_test_steps = 5
+    dimension = weights_model.dimension
+    value = mdrnn.random_sample(out_dim=dimension)
+    for i in range(num_test_steps):
+        value = weights_model.generate(value)
+        assert len(value) == dimension
+        value = mdrnn.proc_generated_touch(value, dimension)
+        assert len(value) == dimension
+
+
+def test_dummy_model():
+    model = mdrnn.DummyMDRNN(Path("/"), 4, 64, 5, 2)
+    dimension = model.dimension
+    value = mdrnn.random_sample(out_dim=dimension)
+    value = model.generate(value)
+    assert len(value) == dimension
+
